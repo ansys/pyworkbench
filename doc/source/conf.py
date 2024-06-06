f0462b084652343e8ff5f5e2c7e8d939547ecc32
Frank Li <frank.li@ansys.com>
2024-06-06 18:03:57 +0100

    quick fix

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -122,7 +122,7 @@ suppress_warnings = ["autoapi.python_import_resolution", "config.cache"]
 exclude_patterns = [
     "**/ExampleData*",
     "**/LaunchWorkbench*",
-    "**/ConnectWorkbench*",
+    "**/ClientWrapper*",
 ]
 
 # additional logos for the latex coverpage
