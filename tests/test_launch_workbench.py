ea50404892a7e8961c4aedfd4367ba32f3abd68a
Frank Li <frank.li@ansys.com>
2024-10-18 19:23:30 +0100

    test machine installs 241?

--- tests/test_launch_workbench.py
+++ tests/test_launch_workbench.py
@@ -34,7 +34,7 @@ def workbench():
     """Launch workbench."""
     workdir = pathlib.Path(__file__).parent
     wb = launch_workbench(
-        version="242",
+        version="241",
         server_workdir=str(workdir.absolute()),
         client_workdir=str(workdir.absolute()),
     )
